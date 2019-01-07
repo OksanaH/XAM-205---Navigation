c2b067b5c708c239661b6939d2ebd76d0a33b733
Oxie <oxieking83@gmail.com>
2019-01-07 10:55:23 +0000

    added completed tasks

---
+++ Exercise 2/Completed/Astronomy/Pages/PersonalisePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+using System;
+
+namespace Astronomy.Pages
+{
+    public partial class PersonalisePage : ContentPage
+    {
+        public PersonalisePage()
+        {
+            InitializeComponent();
+
+            btnSave.Clicked += BtnSaveClicked;
+            btnCancel.Clicked += BtnCancelClicked;
+        }
+
+        async void BtnCancelClicked(object sender, EventArgs e)
+        {
+           await  Navigation.PopModalAsync();
+        }
+
+        async void BtnSaveClicked(object sender, EventArgs e)
+        {
+            Application.Current.Properties["Name"] = entryName.Text;
+            Application.Current.Properties["Icon"] = pickerIcon.SelectedItem;
+            await Application.Current.SavePropertiesAsync();
+            await this.Navigation.PopModalAsync();
+        }
+    }
+}
